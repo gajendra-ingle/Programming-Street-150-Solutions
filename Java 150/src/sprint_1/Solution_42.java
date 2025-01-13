7c851b5323f1d536eace844134e4caff4edfd658
gajendra-ingle <gajendraingle01@gmail.com>
2025-01-13 09:04:24 +0530

    Solution_48 added

--- Java 150/src/sprint_1/Solution_42.java
+++ Java 150/src/sprint_1/Solution_42.java
@@ -11,4 +11,23 @@ package sprint_1;
 
 public class Solution_42 {
 
+	public static void main(String[] args) {
+		int number = 5;
+		for (int i = 0; i < number; i++) {
+			System.out.print(fib(i) + " ");
+		}
+	}
+
+	static int fib(int n) {
+		if (n == 0) {
+			return 0;
+		}
+
+		if (n == 1 || n == 2) {
+			return 1;
+		}
+
+		return fib(n - 1) + fib(n - 2);
+	}
+
 }
--- Java 150/src/sprint_1/Solution_48.java
+++ Java 150/src/sprint_1/Solution_48.java
@@ -11,4 +11,26 @@ package sprint_1;
 
 public class Solution_48 {
 
+	public static void main(String[] args) {
+		System.out.println(sumOfPrimeFactors(12));
+	}
+
+	static int sumOfPrimeFactors(int n) {
+		int sum = 0;
+		if (n % 2 == 0) {
+			sum += 2;
+			while (n % 2 == 0) {
+				n /= 2;
+			}
+		}
+		for (int i = 3; i <= n; i += 2) {
+			if (n % i == 0) {
+				sum += i;
+				while (n % i == 0) {
+					n /= i;
+				}
+			}
+		}
+		return sum;
+	}
 }
